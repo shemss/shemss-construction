d6445b1d6d3a8afe5b32515ee227ce58dada9b82
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2019-05-05 22:03:43 -0700

    Add testimonials carousel

--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -1,7 +1,9 @@
 import videoHero from './home/videoHero'
 import youtubeModal from './home/youtubeModal'
+import textCarousel from './home/textCarousel'
 
 document.addEventListener('DOMContentLoaded', () => {
   videoHero.init()
   youtubeModal()
+  textCarousel()
 })
---
+++ assets/javascripts/home/textCarousel.js
@@ -0,0 +1,35 @@
+const resizeItems = () => {
+  document.querySelectorAll('.carousel-text').forEach(carousel => {
+    const items = carousel.querySelectorAll('.carousel-item')
+    const heights = Array.prototype.map.call(items, item => {
+      item.style.height     = null
+      item.style.visibility = 'hidden'
+      item.style.display    = 'block'
+      const height          = item.offsetHeight
+      item.style.visibility = null
+      item.style.display    = null
+      return height
+    })
+
+    console.log(heights)
+
+    const tallestHeight = Math.max(...heights)
+
+    console.log(tallestHeight)
+
+    items.forEach(item => item.style.height = tallestHeight + 'px')
+  })
+}
+
+let resizeItemsTimeout
+const debounceResizeItems = () => {
+  clearTimeout(resizeItemsTimeout)
+  resizeItemsTimeout = setTimeout(resizeItems, 66)
+}
+
+const textCarousel = () => {
+  resizeItems()
+  addEventListener('resize', debounceResizeItems)
+}
+
+export default textCarousel
