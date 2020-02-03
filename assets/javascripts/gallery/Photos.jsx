aeff8d79de2fa6278215a9523b0992ed476f7add
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2020-02-02 21:46:22 -0800

    Keep photo proportions

--- assets/javascripts/gallery/Photos.jsx
+++ assets/javascripts/gallery/Photos.jsx
@@ -2,32 +2,29 @@ import React from 'react'
 
 import parameterize from './parameterize'
 
-const Photos = props =>
+const Photos = ({ expand, ...props }) =>
   <div style={{ margin: '-1px' }}>
     <div
-      className={`row mx-0 ${props.expand ? '' : 'pr-5 flex-nowrap overflow-auto'}`}
+      className={expand ? '' : 'd-flex pr-5 flex-nowrap overflow-auto'}
     >
       { props.project.filteredPhotos.map((photo, index) =>
-        <div
+        <a
           key={parameterize(photo.file)}
-          className={`${props.expand ? 'col-12' : 'col-12 col-md-6'} px-0`}
-        >
-          <a
-            href={photo.file}
-            onClick={props.toggleExpand}
-            className="d-block"
-            style={{
-              backgroundImage: [ props.expand ? photo.file : null, photo.thumb ]
-              .filter(Boolean).map(p => `url("${p}")`).join(),
-              backgroundPosition: 'center center',
-              backgroundSize:     'cover',
-              border:             '1px solid #fff',
-              paddingTop: props.expand
-              ? `${photo.size[0] / photo.size[1] * 100}%`
-              : '56.25%',
-            }}
-          ></a>
-        </div>
+          href={photo.file}
+          onClick={props.toggleExpand}
+          className="d-block"
+          style={{
+            backgroundImage: [ expand ? photo.file : null, photo.thumb ]
+            .filter(Boolean).map(p => `url("${p}")`).join(),
+            backgroundPosition: 'center center',
+            backgroundSize:     'cover',
+            border:             '1px solid #fff',
+            paddingTop:   expand ? photo.size[0] / photo.size[1] * 100 + '%' : 0,
+            paddingLeft:  expand ? 0 : photo.size[1] / photo.size[0] * 40 + 'vw',
+            width:        expand ? '100%' : 'auto',
+            height:       expand ? 'auto' : '40vw',
+          }}
+        ></a>
       )}
     </div>
   </div>
